8c2ba525b90bd6dee623bebc19ce807d2797bc51
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-13 00:58:03 +0200

    Add useEffect to fetch data from API and update state variables

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,9 +1,33 @@
-import React, {useState} from "react"
+import { response } from "express";
+import React, {useEffect, useState} from "react"
 
 export default function App(){
   const[data, setData] = useState([]);
   const[error, setError] = useState(null);
   const[loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try{
+        constresponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+        if(!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+
+      } catch(error) {
+        setError("Failed to fetch data. Please try again later.");
+      } finally{
+        setLoading(false);
+
+      }
+
+
+    };
+    fetchData();
+
+  }, []);
 
   return(
     <div>
@@ -11,4 +35,3 @@ export default function App(){
     </div>
   );
 }
-export default App
