336505eebf5fcd22739752c6302f089297cfb490
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-13 12:16:49 +0200

    Display error message if data fetching fails and render fetched posts in a list

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,5 +1,8 @@
 import { response } from "express";
 import React, {useEffect, useState} from "react"
+import errorMessage from "./assets/images/error-message.png"
+import BlogPostsImage from"./assets/images/blog-posts.png" 
+
 
 export default function App(){
   const[data, setData] = useState([]);
@@ -9,7 +12,7 @@ export default function App(){
   useEffect(() => {
     const fetchData = async() => {
       try{
-        constresponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         if(!response.ok){
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -29,9 +32,31 @@ export default function App(){
 
   }, []);
 
+  if(loading) {
+    return <div className="loading-div">Loading...</div>;
+
+  }
+  if(error) {
+    return(
+      <div className="error-div">
+        <img src={errorMessage} alt="Error Message" />
+        <p>An error has occurred: {error}</p>
+      </div>
+    );
+  }
+
   return(
     <div>
       <h1>Post</h1>
+    <img src={BlogPostsImage} alt="Blog Posts" />
+    <ol>
+      {data.map((post) => (
+      <li key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </li>
+    ))}
+    </ol>
     </div>
   );
 }
